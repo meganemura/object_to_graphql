93d41d892e19d4256815f1cb26a0b2327c6bab26
meganemura <meganemura@users.noreply.github.com>
2021-12-30 14:10:37 +0900

    Write more tests

--- spec/object_to_graphql_spec.rb
+++ spec/object_to_graphql_spec.rb
@@ -56,4 +56,36 @@ RSpec.describe ObjectToGraphql do
       }
     GRAPHQL
   end
+
+  it "generates a GraphQL query with arguments for nested field" do
+    object = {
+      user: {
+        name: "Alice",
+        email_address: "alice@example.com",
+        accounts: [
+          {name: "alice1"}
+        ]
+      }
+    }
+
+    arguments = [
+      [[:user], {name: "ID", value: "id-1"}],
+      [[:user, :name], {name: "separator", value: ","}],
+      [[:user, :accounts], {name: "order", value: "desc"}]
+    ]
+
+    query = ObjectToGraphql.generate(object, arguments)
+
+    expect(query).to eq(<<~GRAPHQL.chomp)
+      {
+        user(ID: "id-1") {
+          name(separator: ",")
+          emailAddress
+          accounts(order: "desc") {
+            name
+          }
+        }
+      }
+    GRAPHQL
+  end
 end
