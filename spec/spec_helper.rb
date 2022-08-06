8de870fbc10a11fba42e5354597070284e9ba167
meganemura <meganemura@users.noreply.github.com>
2022-08-06 09:29:38 +0900

    Start SimpleCov in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@
 
 require "object_to_graphql"
 
+if ENV["CI"] == "true"
+  require "simplecov"
+
+  SimpleCov.start("rails")
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
