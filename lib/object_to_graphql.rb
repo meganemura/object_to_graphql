4ee59db46b52c530f552f28919fe3ba1a57e564d
meganemura <meganemura@users.noreply.github.com>
2022-08-06 10:16:58 +0900

    Test

--- lib/object_to_graphql.rb
+++ lib/object_to_graphql.rb
@@ -8,6 +8,18 @@ require_relative "object_to_graphql/object_parser"
 require_relative "object_to_graphql/version"
 require_relative "object_to_graphql/nodes"
 
+if false
+  puts ''
+end
+
+puts '' if false
+
+if true
+  puts 't'
+else
+  puts 'f'
+end
+
 module ObjectToGraphQL
   def self.generate(object, arguments = [], variables = [])
     document = ObjectParser.parse(object, arguments, variables)
